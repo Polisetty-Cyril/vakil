e9f2584dca1b448c1d108d10b929fbb102a618a5
ayushwasnothere <ayushwasnothere@gmail.com>
2025-08-24 16:47:44 +0530

    fix main frame in home

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,7 +4,7 @@ import { ButtonWhite } from "./ButtonWhite";
 export const Appbar = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-16 bg-white/70 text-black flex items-center px-[20%] border-b border-border backdrop-blur-sm justify-between">
-      <nav className="flex items-center gap-8">
+      <nav className="flex items-center gap-8 px-10">
         <div className="text-primary font-inter font-bold text-2xl">Vakil</div>
         <ul className="flex items-center gap-4 font-medium text-primary-text">
           <li className="px-4 py-2 hover:bg-shade-1 rounded-sm hover:text-shade-3">
@@ -18,7 +18,7 @@ export const Appbar = () => {
           </li>
         </ul>
       </nav>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 px-10">
         <ButtonWhite label="Sign in" />
         <ButtonAscent label="Try me" />
       </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,8 @@ import { ButtonWhite } from "../components/ButtonWhite";
 
 export function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="mt-10 h-full w-3/5 py-20 px-20 bg-gradient-to-b flex flex-col items-center from-gray-100 to-white border-x border-gray-200">
+    <div className="flex flex-col items-center">
+      <div className="mt-10 w-3/5 py-20 px-20 bg-gradient-to-b flex flex-col items-center from-gray-100 to-white border-x border-gray-200">
         <div className="text-black text-6xl mt-20 font-inter font-bold text-center">
           Shady clauses? Vakil sees through.
         </div>
